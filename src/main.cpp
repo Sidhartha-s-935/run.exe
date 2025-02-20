4025cc86b8e04a4562005cf8834830d2cb0226e6
Sidhartha S <darksoul13570@gmail.com>
2025-02-20 14:55:38 +0530

    feat: run anim
    Still no idle anim just run

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,66 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
 using namespace std;
 using namespace sf;
 
-void movement(Vector2f &position, Sprite &sprite)
+void movement(Vector2f &position, Sprite &runsprite, int &currentFrame, bool &facingRight, Clock &clock)
 {
+    float frameSpeed = 0.1f; // Time between frame updates
+    bool moving = false;
+
     if (Keyboard::isKeyPressed(Keyboard::W))
     {
         position.y -= 0.1;
-        sprite.setPosition(position);
+        moving = true;
     }
     if (Keyboard::isKeyPressed(Keyboard::A))
     {
         position.x -= 0.1;
-        sprite.setPosition(position);
+        facingRight = false;
+        moving = true;
     }
     if (Keyboard::isKeyPressed(Keyboard::S))
     {
         position.y += 0.1;
-        sprite.setPosition(position);
+        moving = true;
     }
     if (Keyboard::isKeyPressed(Keyboard::D))
     {
         position.x += 0.1;
-        sprite.setPosition(position);
+        facingRight = true;
+        moving = true;
+    }
+
+    runsprite.setPosition(position);
+
+    // Update animation frame
+    if (moving && clock.getElapsedTime().asSeconds() > frameSpeed)
+    {
+        currentFrame = (currentFrame + 1) % 8; // Cycle through 8 frames
+        clock.restart();
     }
 }
 
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "SFML");
-    Texture texture;
-    Sprite sprite;
-    texture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/player.png");
-    sprite.setTexture(texture);
-    sprite.setTextureRect(IntRect(0, 0, 55, 37));
+    Texture runtexture;
+    if (!runtexture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/player.png"))
+    {
+        cerr << "Error loading runsprite sheet!" << endl;
+        return -1;
+    }
+
+    Sprite runsprite;
+    runsprite.setTexture(runtexture);
+
+    Vector2f position(400, 300);
+    int frameWidth = 128, frameHeight = 128;
+    int currentFrame = 0;
+    bool facingRight = true;
+    Clock clock;
+
     while (window.isOpen())
     {
         Event event;
@@ -45,10 +71,26 @@ int main()
                 window.close();
             }
         }
-        Vector2f position = sprite.getPosition();
-        movement(position, sprite);
+
+        movement(position, runsprite, currentFrame, facingRight, clock);
+
+        // Set runtexture rect for animation
+        runsprite.setTextureRect(IntRect(currentFrame * frameWidth, 0, frameWidth, frameHeight));
+
+        // Flip runsprite when moving left
+        if (!facingRight)
+        {
+            runsprite.setScale(-1.f, 1.f);
+            runsprite.setOrigin(frameWidth, 0);
+        }
+        else
+        {
+            runsprite.setScale(1.f, 1.f);
+            runsprite.setOrigin(0, 0);
+        }
+
         window.clear();
-        window.draw(sprite);
+        window.draw(runsprite);
         window.display();
     }
 
