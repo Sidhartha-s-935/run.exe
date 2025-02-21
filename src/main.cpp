fef1ab96df5480d527db51a1b6224e7c79ddaf64
Sidhartha S <darksoul13570@gmail.com>
2025-02-21 08:11:01 +0530

    feat: attack anim
    attack animation nothing spl

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,51 @@
 using namespace std;
 using namespace sf;
 
-void movement(Vector2f &position, Sprite &runsprite, int &currentFrame, bool &facingRight, Clock &clock)
+void attack(Sprite &sprite, Texture &attackTexture, int &attackFrame, Clock &attackClock,
+            bool &isAttacking, bool &isRunning, bool facingRight, Vector2f &position, int totalAttackFrames)
 {
-    float frameSpeed = 0.1f; // Time between frame updates
+    float frameSpeed = 0.08f;
+    float attackMoveSpeed = 0.03f; // Distance moved per attack frame
+
+    if (Keyboard::isKeyPressed(Keyboard::K) && !isAttacking)
+    {
+        isAttacking = true;
+        attackFrame = 0; // Start attack animation from frame 0
+        attackClock.restart();
+    }
+
+    if (isAttacking)
+    {
+        if (attackClock.getElapsedTime().asSeconds() > frameSpeed)
+        {
+            attackFrame++; // Move to next attack frame
+            attackClock.restart();
+        }
+
+        if (attackFrame >= totalAttackFrames) // If attack animation is finished
+        {
+            isAttacking = false; // Resume movement
+            attackFrame = 0;     // Reset attack frame
+        }
+
+        sprite.setTexture(attackTexture);
+        sprite.setTextureRect(IntRect(attackFrame * 128, 0, 128, 128));
+
+        // Move forward slightly while attacking
+        if (attackFrame < totalAttackFrames && attackFrame >= 2)
+        {
+            position.x += (facingRight ? attackMoveSpeed : -attackMoveSpeed);
+        }
+    }
+}
+
+void movement(Vector2f &position, Sprite &sprite, Texture &runTexture, Texture &idleTexture,
+              int &currentFrame, bool &facingRight, Clock &clock, bool &isRunning, bool isAttacking)
+{
+    if (isAttacking)
+        return; // Stop movement when attacking
+
+    float frameSpeed = 0.1f;
     bool moving = false;
 
     if (Keyboard::isKeyPressed(Keyboard::W))
@@ -32,12 +74,12 @@ void movement(Vector2f &position, Sprite &runsprite, int &currentFrame, bool &fa
         moving = true;
     }
 
-    runsprite.setPosition(position);
+    sprite.setPosition(position);
+    isRunning = moving;
 
-    // Update animation frame
-    if (moving && clock.getElapsedTime().asSeconds() > frameSpeed)
+    if (clock.getElapsedTime().asSeconds() > frameSpeed)
     {
-        currentFrame = (currentFrame + 1) % 8; // Cycle through 8 frames
+        currentFrame = (currentFrame + 1) % (isRunning ? 8 : 6); // Run: 8 frames, Idle: 6 frames
         clock.restart();
     }
 }
@@ -45,21 +87,26 @@ void movement(Vector2f &position, Sprite &runsprite, int &currentFrame, bool &fa
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "SFML");
-    Texture runtexture;
-    if (!runtexture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/player.png"))
+
+    // Load textures
+    Texture runTexture, idleTexture, attackTexture;
+    if (!runTexture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/run.png") ||
+        !idleTexture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/idle.png") ||
+        !attackTexture.loadFromFile("/home/notsus/projects/c++project/assets/sprites/attack.png"))
     {
-        cerr << "Error loading runsprite sheet!" << endl;
+        cerr << "Error loading sprite sheets!" << endl;
         return -1;
     }
 
-    Sprite runsprite;
-    runsprite.setTexture(runtexture);
+    Sprite sprite;
+    sprite.setTexture(idleTexture);
 
     Vector2f position(400, 300);
     int frameWidth = 128, frameHeight = 128;
-    int currentFrame = 0;
-    bool facingRight = true;
-    Clock clock;
+    int currentFrame = 0, attackFrame = 0;
+    bool facingRight = true, isRunning = false, isAttacking = false;
+    Clock movementClock, attackClock;
+    int totalAttackFrames = 4; // Adjust according to attack animation frames
 
     while (window.isOpen())
     {
@@ -72,25 +119,32 @@ int main()
             }
         }
 
-        movement(position, runsprite, currentFrame, facingRight, clock);
+        attack(sprite, attackTexture, attackFrame, attackClock, isAttacking, isRunning, facingRight, position, totalAttackFrames);
+        movement(position, sprite, runTexture, idleTexture, currentFrame, facingRight, movementClock, isRunning, isAttacking);
 
-        // Set runtexture rect for animation
-        runsprite.setTextureRect(IntRect(currentFrame * frameWidth, 0, frameWidth, frameHeight));
+        // Set texture based on state
+        if (!isAttacking)
+        {
+            sprite.setTexture(isRunning ? runTexture : idleTexture);
+            sprite.setTextureRect(IntRect(currentFrame * frameWidth, 0, frameWidth, frameHeight));
+        }
 
-        // Flip runsprite when moving left
+        // Flip sprite if facing left
         if (!facingRight)
         {
-            runsprite.setScale(-1.f, 1.f);
-            runsprite.setOrigin(frameWidth, 0);
+            sprite.setScale(-2.f, 2.f);
+            sprite.setOrigin(frameWidth, 0);
         }
         else
         {
-            runsprite.setScale(1.f, 1.f);
-            runsprite.setOrigin(0, 0);
+            sprite.setScale(2.f, 2.f);
+            sprite.setOrigin(0, 0);
         }
 
+        sprite.setPosition(position); // Update position after attack movement
+
         window.clear();
-        window.draw(runsprite);
+        window.draw(sprite);
         window.display();
     }
 
